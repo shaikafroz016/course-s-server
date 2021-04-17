2db3db9135d53e5162d674061bcac71af764458b
Shaik Afroz <shaikafroz0016@gmail.com>
2021-04-17 21:13:33 +0000

    admin restricted

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use('/users', usersRouter);
 
 
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build', 'index.html')));
 
 
 app.use('/dishes',dishRouter);
