ec952b299f975dbd866cf12cf8c7c73dcebbd3dd
Shaik Afroz <shaikafroz0016@gmail.com>
2021-04-17 21:07:45 +0000

    admin restricted

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ const uploadRouter = require('./routes/uploadRouter');
 const favoriteRouter = require('./routes/favoriteRouter');
 var commentRouter = require('./routes/commentRouter');
 var urlRouter = require('./routes/urlRouter');
+var cors = require('cors')
 var app = express();
 
-
+app.use(cors())
 //db config
 const db=require('./config/keys').MongoURI;
 
@@ -30,11 +31,21 @@ mongoose.connect(db, { useNewUrlParser:true})
 .then(() => console.log('mongo db connected..'))
 .catch(err => console.log(err));
 
+//serve static assect if it is production
+if ( process.env.NODE_ENV === "production"){
+  app.use(express.static("client/build"));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, "client","build", "index.html"));
+  });
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use('/admin', adminRouter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser('12345-67890-09876-54321'));
@@ -45,7 +56,6 @@ app.use(passport.initialize());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/admin', adminRouter);
 
 
 
